5ee1902078964fb0cd65c4b515c0580919675e26
Justin Conaghan <justin.conaghan@gmail.com>
2013-02-15 11:54:50 +1100

    merge issues resolved

---
+++ src/Prova.Tests/ForTestable/StepArgumentTransformer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace Prova.Tests.ForTestable
+{
+    [Binding]
+    public class StepArgumentTransformer
+    {
+        private static readonly IEnumerable<Type> AllLoadedTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes());
+
+        [StepArgumentTransformation("type (.*)")]
+        public Type TransformToType(string typeName)
+        {
+            return AllLoadedTypes.First(x => x.Name == typeName);
+        } 
+        
+        [StepArgumentTransformation("instance of (.*)")]
+        public dynamic TransformToInstance(string typeName)
+        {
+            return Activator.CreateInstance(TransformToType(typeName));
+        }
+    }
+}
---
+++ src/Prova.Tests/ForTestable/TestableContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prova.Tests.ForTestable
+{
+    public class TestableContext
+    {
+        private Testable _testable;
+        private Testable _otherTestable;
+        private dynamic _instance;
+        private dynamic _secondInstance;
+        private dynamic _expectedDependency;
+        private Exception _exception;
+
+        public Testable Testable
+        {
+            get { return _testable; }
+            set { _testable = value; }
+        }
+
+        public Testable OtherTestable
+        {
+            get { return _otherTestable; }
+            set { _otherTestable = value; }
+        }
+
+        public dynamic Instance
+        {
+            get { return _instance; }
+            set { _instance = value; }
+        }
+
+        public dynamic SecondInstance
+        {
+            get { return _secondInstance; }
+            set { _secondInstance = value; }
+        }
+
+        public dynamic ExpectedDependency
+        {
+            get { return _expectedDependency; }
+            set { _expectedDependency = value; }
+        }
+
+        public Exception Exception
+        {
+            get { return _exception; }
+            set { _exception = value; }
+        }
+    }
+}
